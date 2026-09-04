ca681ae4b100e4b2cea7c36ac7cb657b1292e663
xuan <xy@jxwxxkj.com>
2026-09-04 11:28:53 +0800

    nacos配置中心及服务注册发现接入

--- src/main/java/com/xuan/userservice/UserServiceApplication.java
+++ src/main/java/com/xuan/userservice/UserServiceApplication.java
@@ -3,9 +3,13 @@ package com.xuan.userservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.xuan.userservice.mapper")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
     public static void main(String[] args) {
